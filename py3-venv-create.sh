845d32261405f5899e06726d271028c81c25e081
ahirner <a.hirner@gmail.com>
2017-03-13 17:51:25 +0100

    virtualenv

---
+++ py3-venv-create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+directory=${1:-"py3-ml"}
+
+echo "Installing dev packages"
+sudo apt-get install python3-pip python3-dev python-virtualenv 
+
+echo "Creating python3 virtual environment in $directory..."
+virtualenv3 -p python3 --system-site-packages $direcory
+
+echo "change to virtualenv with: source ./$directory/bin/activate"
