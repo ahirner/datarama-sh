df30459bb29e947f847831282e72c2c14a8c3bcd
ahirner <a.hirner@gmail.com>
2017-03-13 22:31:16 +0100

    added opening 10 default ports

--- create_vm_az.sh
+++ create_vm_az.sh
@@ -10,3 +10,6 @@ ssh-keygen -f ~/.ssh/$key -t rsa -b 2048 -C '' -N ''
 pub=$key".pub"
 az vm create --resource-group $rg --name $vm --image Canonical:UbuntuServer:16.04-LTS:latest --size Standard_NC6 --admin-username $vm --ssh-key-value ~/.ssh/$pub
 
+echo "opening ports 8889-8899"
+az vm open-port --port 8889-8899 -g $rg -n $vm
+
