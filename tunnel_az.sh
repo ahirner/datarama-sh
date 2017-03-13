763cfe33ec781551aa0a18ac09256e5135eb2fe5
ahirner <a.hirner@gmail.com>
2017-03-13 23:41:41 +0100

    added tunneling of reserved ports

---
+++ tunnel_az.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rg=$1
+vm=$2
+
+key=$rg"_"$vm"_id_rsa"
+
+remote_begin=8889
+remote_end=8899
+offset_local=1000
+ip=$(az vm list-ip-addresses -g $rg -n $vm |  python -c "import json,sys;obj=json.load(sys.stdin);print obj[0]['virtualMachine']['network']['publicIpAddresses'][0]['ipAddress'];")
+
+echo "tunneling ports $remote_begin-$remote_end to +$offset_local locally"
+echo "interrupt to close tunnels"
+Ls=$(echo $remote_begin-$remote_end | python -c "import sys; p1, p2 = sys.stdin.read().split('-'); print(' '.join(['-L ' + str(p)+':localhost:'+str(p+$offset_local) for p in range(int(p1), int(p2)) ])) ")
+ssh -N $Ls $vm@$ip -i ~/.ssh/$key
