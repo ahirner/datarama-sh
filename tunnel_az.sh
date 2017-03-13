9c34d4e547ba1eeefc695e7cdafd0896f643f978
ahirner <a.hirner@gmail.com>
2017-03-13 23:59:04 +0100

    fixed mixed up ports remote vs local

--- tunnel_az.sh
+++ tunnel_az.sh
@@ -11,5 +11,5 @@ ip=$(az vm list-ip-addresses -g $rg -n $vm |  python -c "import json,sys;obj=jso
 
 echo "tunneling ports $remote_begin-$remote_end to +$offset_local locally"
 echo "interrupt to close tunnels"
-Ls=$(echo $remote_begin-$remote_end | python -c "import sys; p1, p2 = sys.stdin.read().split('-'); print(' '.join(['-L ' + str(p)+':localhost:'+str(p+$offset_local) for p in range(int(p1), int(p2)) ])) ")
+Ls=$(echo $remote_begin-$remote_end | python -c "import sys; p1, p2 = sys.stdin.read().split('-'); print(' '.join(['-L ' + str(p+$offset_local)+':localhost:'+str(p) for p in range(int(p1), int(p2)) ])) ")
 ssh -N $Ls $vm@$ip -i ~/.ssh/$key
