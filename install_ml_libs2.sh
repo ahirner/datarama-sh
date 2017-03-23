292db4640fd74d1baefbe7a249d076cc77fc908d
ahirner <a.hirner@gmail.com>
2017-03-23 14:04:42 +0100

    more ml libs

---
+++ install_ml_libs2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Convenience
+
+pip install theano
+echo "[global]
+device = gpu
+floatX = float32" > ~/.theanorc
+
+pip install keras
+mkdir ~/.keras
+echo '{
+    "image_dim_ordering": "th",
+    "epsilon": 1e-07,
+    "floatx": "float32",
+    "backend": "theano"
+}' > ~/.keras/keras.json
+
+#Probabilistic
+pip install git+https://github.com/pymc-devs/pymc3
+pip install pystan
+pip install edward
+pip install pomegranate
