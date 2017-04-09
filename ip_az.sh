e4c27d501db60be5a22baefe3e1bfca33b21d592
ahirner <a.hirner@gmail.com>
2017-04-09 15:00:40 +0200

    update to python 3 print

--- ip_az.sh
+++ ip_az.sh
@@ -4,6 +4,6 @@ vm=$2
 
 key=$rg"_"$vm"_id_rsa"
 
-ip=$(az vm list-ip-addresses -g $rg -n $vm |  python -c "import json,sys;obj=json.load(sys.stdin);print obj[0]['virtualMachine']['network']['publicIpAddresses'][0]['ipAddress'];")
+ip=$(az vm list-ip-addresses -g $rg -n $vm |  python -c "import json,sys;obj=json.load(sys.stdin);print(obj[0]['virtualMachine']['network']['publicIpAddresses'][0]['ipAddress']);")
 echo "$ip"
 
