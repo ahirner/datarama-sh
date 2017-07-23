fca0e2f4eb153cc3be73a82574ed20f7a79cc900
ahirner <a.hirner@gmail.com>
2017-07-23 21:15:30 +0200

    OpenCV from source

---
+++ install_opencv_primer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Modified from https://github.com/milq/milq/blob/master/scripts/bash/install-opencv.sh
+#Copyright (c) 2017 Manuel Ignacio López Quintero
+
+# Build tools:
+sudo apt-get install -y build-essential cmake
+
+# Media I/O:
+sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+
+# Video I/O:
+sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+
+# Parallelism and linear algebra libraries:
+sudo apt-get install -y libtbb-dev libeigen3-dev
