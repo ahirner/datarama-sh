0a4f02edd37b6a2fe4664c86475d485554a00aeb
ahirner <a.hirner@gmail.com>
2017-07-23 22:54:54 +0200

    Sensible miniconda

---
+++ install_miniconda.sh
@@ -0,0 +1,12 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+
+./Miniconda3-latest-Linux-x86_64.sh
+conda create --name fat-ml
+
+echo "source activate fat-ml" >> .bashrc
+source .bashrc
+
+conda enf update -f=./environment.yml
+
+rm Miniconda3-latest-Linux-x86_64.sh
